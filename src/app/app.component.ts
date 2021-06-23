15f73f20c0ac460766a1c1292d2bc761c58dd60a
David Rodriguez <David>
2021-06-23 12:33:14 -0500

    Initialize Repository

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angProject1';
+  title = 'Gamers Website';
+
+  reviewerform = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    telephone: new FormControl('', [Validators.required, Validators.maxLength(10)]),
+    yearsPlaying: new FormControl('', Validators.required),
+    genre: new FormControl('', Validators.required),
+    favorite: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    previously: new FormControl('', Validators.required),
+    list: new FormControl('', Validators.required),
+    writing: new FormControl('', Validators.required)
+
+  })
 }
